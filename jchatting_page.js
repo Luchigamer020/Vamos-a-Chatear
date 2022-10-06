dbfd7662cc47b7f7ac21f6a2cd98fc7774b3b773
Luchigamer020 <106202621+Luchigamer020@users.noreply.github.com>
2022-10-05 19:45:32 -0500

    Update jchatting_page.js

--- jchatting_page.js
+++ jchatting_page.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyDMzmoinQujYBotFaT0tA7fPUPRju5c-Yc",
-  authDomain: "vamos-a-chatear.firebaseapp.com",
-  databaseURL: "https://vamos-a-chatear-default-rtdb.firebaseio.com",
-  projectId: "vamos-a-chatear",
-  storageBucket: "vamos-a-chatear.appspot.com",
-  messagingSenderId: "1064086224667",
-  appId: "1:1064086224667:web:e282cfc5f1c87636819dbe"
+  apiKey: "AIzaSyDOOxE7dHeUg19PwjiZP73oIoE7SEd3JwQ",
+  authDomain: "chatear1-89e9e.firebaseapp.com",
+  databaseURL: "https://chatear1-89e9e-default-rtdb.firebaseio.com",
+  projectId: "chatear1-89e9e",
+  storageBucket: "chatear1-89e9e.appspot.com",
+  messagingSenderId: "646314372390",
+  appId: "1:646314372390:web:27a8e8beccb15eb36365e7"
 };
 
   
