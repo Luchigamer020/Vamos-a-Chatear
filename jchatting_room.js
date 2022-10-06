c609c3f1661b0ede146043c92306e134fb2a4635
Luchigamer020 <106202621+Luchigamer020@users.noreply.github.com>
2022-10-05 19:38:35 -0500

    Update jchatting_room.js

--- jchatting_room.js
+++ jchatting_room.js
@@ -1,14 +1,15 @@
 
 //AÑADE TUS ENLACES DE FIREBASE
 var firebaseConfig = {
-      apiKey: "AIzaSyDMzmoinQujYBotFaT0tA7fPUPRju5c-Yc",
-      authDomain: "vamos-a-chatear.firebaseapp.com",
-      databaseURL: "https://vamos-a-chatear-default-rtdb.firebaseio.com",
-      projectId: "vamos-a-chatear",
-      storageBucket: "vamos-a-chatear.appspot.com",
-      messagingSenderId: "1064086224667",
-      appId: "1:1064086224667:web:e282cfc5f1c87636819dbe"
-    };
+  apiKey: "AIzaSyDMzmoinQujYBotFaT0tA7fPUPRju5c-Yc",
+  authDomain: "vamos-a-chatear.firebaseapp.com",
+  databaseURL: "https://vamos-a-chatear-default-rtdb.firebaseio.com",
+  projectId: "vamos-a-chatear",
+  storageBucket: "vamos-a-chatear.appspot.com",
+  messagingSenderId: "1064086224667",
+  appId: "1:1064086224667:web:e282cfc5f1c87636819dbe"
+};
+
     
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
